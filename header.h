a49c0e3c81fab97d14043ccd713f55bd212ba5da
lobak <lobak070811@gmail.com>
2026-08-30 16:48:50 +0800

    upload header file

---
+++ header.h
@@ -0,0 +1,27 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+#include <iostream>
+#include <string>
+
+//declare global variable
+extern std::string currentID;
+extern std::string currentName;
+extern std::string currentPhone;
+extern std::string currentRole;
+
+void mainMenu();
+
+void registerMenu();
+void studentRegister();
+void ownerRegister();
+void agentRegister();
+
+void loginMenu();
+
+void ownerPropertyMenu();
+void agentPropertyMenu();
+void addProperty();
+void editProperty();
+
+#endif //end code
