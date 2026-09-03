60dbf4be530edf1794bd6744b54d85d48a1bdb12
lobak <lobak070811@gmail.com>
2026-09-03 17:33:37 +0800

    add student menu

---
+++ student_menu.cpp
@@ -0,0 +1,52 @@
+#include "header.h"
+
+#include <iostream>
+
+using namespace std;
+
+void studentMenu()//main function
+{
+    int choice;
+
+    do
+    {
+        cout << "\n========================================" << endl;
+        cout << "              STUDENT MENU" << endl;
+        cout << "========================================" << endl;
+        cout << "1. View Property" << endl;
+        cout << "2. Search Property" << endl;
+        cout << "3. Add Shortlist" << endl;
+        cout << "4. View Shortlist" << endl;
+        cout << "5. Logout" << endl;
+        cout << "========================================" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice)
+        {
+            case 1:
+                viewPropertyStudent();
+                break;
+
+            case 2:
+                //searchProperty();
+                break;
+
+            case 3:
+                addShortlist();
+                break;
+                
+            case 4:
+            	//viewShortListStudent();
+            	break;
+
+            case 5:
+                cout << "\nLogging out..." << endl;
+                break;
+
+            default:
+                cout << "\nInvalid choice. Please try again." << endl;
+        }
+
+    } while(choice != 5);
+}
